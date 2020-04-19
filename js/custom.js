5eb48c32d2f41734a99b8823c36930f297d13f28
Kasumi <amfever@chenmianmiandeMacBook-Air.local>
2020-04-19 15:45:34 +0900

    add new work

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,48 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // header color change
-  $(window).on("scroll", function() {
+  $(window).on('scroll', function () {
     var scrollDistance = $(window).scrollTop();
-    var $header = $(".js-header");
+    var $header = $('.js-header');
     if (scrollDistance > 80) {
-      $header.addClass("header--colored");
+      $header.addClass('header--colored');
     } else {
-      $header.removeClass("header--colored");
+      $header.removeClass('header--colored');
     }
   });
 
   // wow 初始化
   new WOW().init();
 });
+//slide
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName('mySlides');
+  var dots = document.getElementsByClassName('dot');
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' active';
+}
